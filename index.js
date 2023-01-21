31772e19bf93f5faf3b9b8670db9d6839209108f
suraj.be <suraj.be@media.net>
2023-01-21 10:22:46 +0530

    done with application level route

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const app = express()
 require('./scripts/colors')
 const port = 5001
 
+/* Application Level Middleware */
+const loggerMiddleware = (req, res, next) => {
+  console.log(`${new Date()} --- Request [${req.method}] [${req.url}]`);
+  next()
+}
+
+app.use(loggerMiddleware)
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`.success);
 })
