170b44bae8d6483e2beb69cdec1035af9945b69b
suraj.be <suraj.be@media.net>
2023-01-21 10:51:48 +0530

    Implemented error-handler-middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.clear()
 const express = require('express')
 const app = express()
+const router = express.Router()
 require('./scripts/colors')
 const port = 5001
 
@@ -15,13 +16,13 @@ app.use(loggerMiddleware)
 
 /* Router Level Middleware */
 const fakeAuth = (req, res, next) => {
-  const authStatus = true
+  const authStatus = false
   if (authStatus) {
     console.log('User AuthStatus: ', authStatus)
     next()
   } else {
     res.status(401)
-    throw new Error('User is not authorized')
+    throw new Error('User not authorized')
   }
 }
 const getUsers = (req, res) => {
@@ -36,6 +37,39 @@ app.use('/api/users', router)
 
 
 
+/* Error Handler Middleware */
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500
+  switch (statusCode) {
+    case 401:
+      res.json({
+        title: 'Unauthorized',
+        message: err.message
+      })
+      break
+
+    case 404:
+      res.json({
+        title: 'Not Found',
+        message: err.message
+      })
+      break
+
+    case 500:
+      res.json({
+        title: 'Server Error',
+        message: err.message
+      })
+      break
+
+    default:
+      break
+  }
+}
+
+
+
+app.use(errorHandler)
 app.listen(port, () => {
   console.log(`Server started on port ${port}`.success);
 })
